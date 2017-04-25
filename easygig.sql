21b88dfb5aa2f7a22495a6c299d9cd64f93c03bb
root <zcfilosi@uga.edu>
2017-04-24 20:02:49 -0400

    added easygig.sql for nate

--- easygig.sql
+++ easygig.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.30, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: easygig
 -- ------------------------------------------------------
--- Server version	5.7.17-log
+-- Server version	5.6.30-1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,188 +16,146 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `bands`
+-- Table structure for table `account_info`
 --
 
-DROP TABLE IF EXISTS `bands`;
+DROP TABLE IF EXISTS `account_info`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bands` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `band_name` varchar(50) NOT NULL,
-  `description` varchar(300) DEFAULT NULL,
-  `contact_name` varchar(50) NOT NULL,
-  `telephone` varchar(15) NOT NULL,
+CREATE TABLE `account_info` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `username` varchar(30) DEFAULT NULL,
+  `pw` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `applications`
+--
+
+DROP TABLE IF EXISTS `applications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `applications` (
+  `id` int(11) DEFAULT NULL,
+  `event_id` int(11) DEFAULT NULL,
+  `band_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `band`
+--
+
+DROP TABLE IF EXISTS `band`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `band` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `band_name` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `telephone` varchar(12) DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
-  `facebook` varchar(50) DEFAULT NULL,
-  `twitter` varchar(50) DEFAULT NULL,
+  `facebook` varchar(30) DEFAULT NULL,
+  `twitter` varchar(30) DEFAULT NULL,
   `soundcloud` varchar(50) DEFAULT NULL,
   `youtube` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bands`
+-- Table structure for table `band_post`
 --
 
-LOCK TABLES `bands` WRITE;
-/*!40000 ALTER TABLE `bands` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bands` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `venues`
---
-
-DROP TABLE IF EXISTS `venues`;
+DROP TABLE IF EXISTS `band_post`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `venues` (
+CREATE TABLE `band_post` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `band_id` int(11) DEFAULT NULL,
+  `post_title` varchar(50) DEFAULT NULL,
+  `post_content` varchar(200) DEFAULT NULL,
+  `date_posted` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `events` (
+  `id` int(11) DEFAULT NULL,
+  `venue_id` int(11) DEFAULT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `timeandplace` varchar(30) DEFAULT NULL,
+  `isFilled` tinyint(1) DEFAULT NULL,
+  `band_id` int(11) DEFAULT NULL,
+  `description` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `venue_name` varchar(50) NOT NULL,
-  `description` varchar(300) DEFAULT NULL,
-  `contact_name` varchar(50) NOT NULL,
-  `telephone` varchar(15) NOT NULL,
+  `sender_name` varchar(50) DEFAULT NULL,
+  `recipient_name` varchar(50) DEFAULT NULL,
+  `sender_id` int(11) DEFAULT NULL,
+  `recipient_id` int(11) DEFAULT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `contents` varchar(256) DEFAULT NULL,
+  `time_sent` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `venue`
+--
+
+DROP TABLE IF EXISTS `venue`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `venue` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `venue_name` varchar(50) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `telephone` varchar(12) DEFAULT NULL,
   `email` varchar(30) DEFAULT NULL,
-  `website` varchar(50) DEFAULT NULL,
-  `facebook` varchar(50) DEFAULT NULL,
-  `twitter` varchar(50) DEFAULT NULL,
+  `facebook` varchar(30) DEFAULT NULL,
+  `twitter` varchar(30) DEFAULT NULL,
+  `youtube` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `venues`
+-- Table structure for table `venue_post`
 --
 
-LOCK TABLES `venues` WRITE;
-/*!40000 ALTER TABLE `venues` DISABLE KEYS */;
-/*!40000 ALTER TABLE `venues` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `UserAccount`
---
-
-DROP TABLE IF EXISTS `UserAccount`;
+DROP TABLE IF EXISTS `venue_post`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `UserAccount` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `band_venue_id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+CREATE TABLE `venue_post` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `venue_id` int(11) DEFAULT NULL,
+  `post_title` varchar(50) DEFAULT NULL,
+  `post_content` varchar(200) DEFAULT NULL,
+  `date_posted` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `UserAccount`
---
-
-LOCK TABLES `UserAccount` WRITE;
-/*!40000 ALTER TABLE `UserAccount` DISABLE KEYS */;
-/*!40000 ALTER TABLE `UserAccount` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `BandAccounts`
---
-
-DROP TABLE IF EXISTS `BandAccounts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `BandAccounts` (
-  `user_id` int(11) NOT NULL,
-  `band_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `BandAccounts`
---
-
-LOCK TABLES `BandAccounts` WRITE;
-/*!40000 ALTER TABLE `BandAccounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `BandAccounts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `VenueAccounts`
---
-
-DROP TABLE IF EXISTS `VenueAccounts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `VenueAccounts` (
-  `user_id` int(11) NOT NULL,
-  `venue_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `VenueAccounts`
---
-
-LOCK TABLES `VenueAccounts` WRITE;
-/*!40000 ALTER TABLE `BandAccounts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `BandAccounts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `bandpost`
---
-
-DROP TABLE IF EXISTS `bandpost`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bandpost` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `band_id` int(11) NOT NULL,
-  `post_title` varchar(30) NOT NULL,
-  `post_content` varchar(140) NOT NULL,
-  'date_posted' DATE TIME ,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `login`
---
-
-LOCK TABLES `bandpost` WRITE;
-/*!40000 ALTER TABLE `bandpost` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bandpost` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `bandpost`
---
-
-DROP TABLE IF EXISTS `venuepost`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `venuepost` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `venue_id` int(11) NOT NULL,
-  `post_title` varchar(30) NOT NULL,
-  `post_content` varchar(140) NOT NULL,
-  'date_posted' DATETIME DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `venuepost`
---
-
-LOCK TABLES `venuepost` WRITE;
-/*!40000 ALTER TABLE `venuepost` DISABLE KEYS */;
-/*!40000 ALTER TABLE `venuepost` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -208,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-07 16:11:33
+-- Dump completed on 2017-04-24 20:00:07
